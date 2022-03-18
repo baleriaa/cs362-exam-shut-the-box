ffd3ffe8937296bcfe61fd528b01a0dc5a599f03
Baleria <breyes815@gmail.com>
2022-03-17 19:10:10 -0700

    tile length tests complete

--- lib/tile_set.rb
+++ lib/tile_set.rb
@@ -1,6 +1,11 @@
 require_relative 'tile'
 
 class TileSet
+  attr_reader :length
+
+  def initialize(length = 9)
+    @length = length
+  end
 
   def self.generate(quantity = 9)
     (1..quantity).map { |n| Tile.new(n) }
--- spec/tile_set_spec.rb
+++ spec/tile_set_spec.rb
@@ -5,11 +5,13 @@ describe 'A tile set' do
   describe '::generate' do
 
     it 'returns an array of 9 tiles by default' do
-      raise "Start here"
+      tile_set = TileSet.new()
+      expect(tile_set.length).to eq(9)
     end
 
     it 'returns an array of tiles of a specified length' do
-      skip
+      tile_set = TileSet.new(10)
+      expect(tile_set.length).to eq(10)
     end
 
     it 'returns an array of tiles whose values increase from 1, by 1' do
